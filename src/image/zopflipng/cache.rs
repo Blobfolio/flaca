e49e580dd601fbeeed41984a78da92dbdc0b7b03
Josh Stoik <josh@blobfolio.com>
2024-04-21 18:20:33 -0700

    cleanup: convert some more bound panics to Err

--- src/image/zopflipng/cache.rs
+++ src/image/zopflipng/cache.rs
@@ -96,9 +96,10 @@ impl MatchCache {
 		length: &mut u16,
 	) -> Result<bool, ZopfliError> {
 		// One sanity check to rule them all.
-		if pos >= self.ld.len() || SUBLEN_CACHED_LEN * pos >= self.sublen.len() {
-			return Err(zopfli_error!());
-		}
+		if pos >= self.ld.len() { return Err(zopfli_error!()); }
+		let cache_sublen: &[u8; SUBLEN_CACHED_LEN] = self.sublen.get(SUBLEN_CACHED_LEN * pos..SUBLEN_CACHED_LEN * pos + SUBLEN_CACHED_LEN)
+			.and_then(|s| s.try_into().ok())
+			.ok_or(zopfli_error!())?;
 
 		// If we have no distance, we have no cache.
 		let (cache_len, cache_dist) = self.ld[pos];
@@ -107,7 +108,7 @@ impl MatchCache {
 		// Find the max sublength once, if ever.
 		let maxlength =
 			if sublen.is_none() { 0 }
-			else { max_sublen(&self.sublen[SUBLEN_CACHED_LEN * pos..]) };
+			else { max_sublen(cache_sublen) };
 
 		// Proceed if our cached length or max sublength are under the limit.
 		if
@@ -120,15 +121,13 @@ impl MatchCache {
 			if sublen.is_none() || usize::from(cache_len) <= maxlength {
 				// Cap the length.
 				*length = cache_len;
-				if usize::from(*length) > *limit {
-					*length = *limit as u16;
-				}
+				if usize::from(*length) > *limit { *length = *limit as u16; }
 
 				// Set the distance from the sublength cache.
 				if let Some(s) = sublen {
 					// Pull the sublength from cache and pull the distance from
 					// that.
-					self.write_sublen(pos, usize::from(*length), s);
+					if 3 <= *length { write_sublen(cache_sublen, s); }
 					*distance = s[usize::from(*length)];
 
 					// Sanity check: make sure the sublength distance at length
@@ -142,9 +141,7 @@ impl MatchCache {
 					}
 				}
 				// Use the cached distance directly.
-				else {
-					*distance = cache_dist;
-				}
+				else { *distance = cache_dist; }
 
 				// We did stuff!
 				return Ok(true);
@@ -185,6 +182,12 @@ impl MatchCache {
 			return Ok(());
 		}
 
+		// Reslice it to the (inclusive) length, ignoring the first 3 entries
+		// since they're below the minimum give-a-shittable limit. Note that
+		// without them, each index can be represented (and stored) as a u8.
+		let slice = sublen.get(ZOPFLI_MIN_MATCH..=usize::from(length))
+			.ok_or(zopfli_error!())?;
+
 		// Save the length/distance bit.
 		if distance == 0 { return Err(zopfli_error!()); }
 		self.ld[pos] = (length, distance);
@@ -196,50 +199,26 @@ impl MatchCache {
 			.take(ZOPFLI_CACHE_LENGTH);
 
 		// Start by writing all mismatched pairs, up to the limit.
-		for (i, pair) in sublen.windows(2).skip(ZOPFLI_MIN_MATCH).take(usize::from(length) - 3).enumerate() {
+		for (i, pair) in (0_u8..).zip(slice.windows(2)) {
 			if pair[0] != pair[1] {
-				let Some(next) = dst.next() else { return Ok(()); };
-				next[0] = i as u8;
-				next[1..].copy_from_slice(pair[0].to_le_bytes().as_slice());
+				let Some([d0, d1, d2]) = dst.next() else { return Ok(()); };
+				*d0 = i;
+				[*d1, *d2] = pair[0].to_le_bytes();
 			}
 		}
 
 		// The final value is implicitly "mismatched"; if we haven't hit the
 		// limit we should write it too.
-		if let Some(next) = dst.next() {
-			next[0] = (length - 3) as u8;
-			next[1..].copy_from_slice(sublen[usize::from(length)].to_le_bytes().as_slice());
+		if let Some([d0, d1, d2]) = dst.next() {
+			*d0 = (length - 3) as u8;
+			[*d1, *d2] = slice[slice.len() - 1].to_le_bytes();
 
-			// If we're still below the limit, copy (just) the length to the
+			// If we're still below the limit, copy (only) the length to the
 			// last slot to simplify any subsequent max_length lookups.
-			if let Some([c1, _rest @ ..]) = dst.last() {
-				*c1 = (length - 3) as u8;
-			}
+			if let Some([d0, _, _]) = dst.last() { *d0 = (length - 3) as u8; }
 		}
 
 		Ok(())
-	}
-
-	/// # Write Sublength.
-	///
-	/// Fill the provided sublength slice with data from the cache.
-	fn write_sublen(&self, pos: usize, length: usize, sublen: &mut [u16; SUBLEN_LEN]) {
-		// Short circuit.
-		if length < ZOPFLI_MIN_MATCH { return; }
-
-		let slice = &self.sublen[SUBLEN_CACHED_LEN * pos..SUBLEN_CACHED_LEN * pos + SUBLEN_CACHED_LEN];
-		let maxlength = max_sublen(slice);
-		let mut prevlength = 0;
-
-		for chunk in slice.chunks_exact(3) {
-			let length = usize::from(chunk[0]) + ZOPFLI_MIN_MATCH;
-			if prevlength <= length {
-				let dist = u16::from_le_bytes([chunk[1], chunk[2]]);
-				sublen[prevlength..=length].fill(dist);
-			}
-			if length == maxlength { return; }
-			prevlength = length + 1;
-		}
 	}
 }
 
@@ -248,7 +227,26 @@ impl MatchCache {
 /// # Max Sublength.
 ///
 /// Return the maximum sublength length for a given chunk.
-const fn max_sublen(slice: &[u8]) -> usize {
-	if slice.len() < SUBLEN_CACHED_LEN || (slice[1] == 0 && slice[2] == 0) { 0 }
+const fn max_sublen(slice: &[u8; SUBLEN_CACHED_LEN]) -> usize {
+	// If the first chunk has no distance, assume a zero length.
+	if slice[1] == 0 && slice[2] == 0 { 0 }
+	// Otherwise the "max" is stored as the first value of the last chunk.
 	else { slice[SUBLEN_CACHED_LEN - 3] as usize + 3 }
+}
+
+/// # Write Sublength.
+///
+/// Fill the provided sublength slice with data from the cache.
+fn write_sublen(src: &[u8; SUBLEN_CACHED_LEN], dst: &mut [u16; SUBLEN_LEN]) {
+	let maxlength = max_sublen(src);
+	let mut old = 0;
+	for chunk in src.chunks_exact(3) {
+		let length = usize::from(chunk[0]) + ZOPFLI_MIN_MATCH;
+		if old <= length {
+			let value = u16::from_le_bytes([chunk[1], chunk[2]]);
+			dst[old..=length].fill(value);
+		}
+		if length == maxlength { return; }
+		old = length + 1;
+	}
 }
